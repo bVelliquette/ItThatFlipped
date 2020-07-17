0ae9df845ba2771431242136b085ddef444d46f9
bVelliquette <BenjaminVelliquette@gmail.com>
2020-07-17 19:14:42 -0400

    Inital functionality

---
+++ ItThatFlipped/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using NinjaData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItThatFlipped
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private async void LoadBreachstones(object sender, RoutedEventArgs e)
+        {
+            await BreachstonePriceProcessor.InitLoadAsync();
+            List<Tuple<string, float>> stones = BreachstonePriceProcessor.ProfitCalc();
+            string temp = "";
+            foreach (var item in stones)
+            {
+                temp += $"{item.Item1, -40}{item.Item2}\n";
+            }
+            TestBox.Text = temp;
+        }
+
+        private void TestClear(object sender, RoutedEventArgs e)
+        {
+            TestBox.Text = "";
+        }
+
+        private async void UpdateBreachstones(object sender, RoutedEventArgs e)
+        {
+            await BreachstonePriceProcessor.LoadData();
+            LoadBreachstones(sender, e);
+        }
+    }
+}
---
+++ NinjaData/BreachstoneListingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaData
+{
+    public class BreachstoneListingModel
+    {
+        public List<Fragment> Lines { get; set; }
+    }
+
+    public class Fragment
+    {
+        public string CurrencyTypeName { get; set; }
+        public float chaosEquivalent { get; set; }
+    }
+}
